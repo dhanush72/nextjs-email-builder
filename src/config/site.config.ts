ee277b43356953d34bb6133d029e9d7d7ebaef36
dhanush72 <dhanuraj7258@gmail.com>
2024-02-17 12:17:40 +0530

    ✨ Added dynamic page title and description config

---
+++ src/config/site.config.ts
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+
+enum MODE {
+  DARK = "dark",
+  LIGHT = "light",
+}
+
+export const siteConfig = {
+  title: "Email Builder",
+  description:
+    "Next.js Email Builder simplifies the process of creating and customizing email templates within Next.js applications, empowering developers and designers to deliver engaging and visually appealing email communications efficiently.",
+  logo: "",
+  icon: "",
+  mode: MODE.LIGHT,
+};
+
+export const metaObject = (
+  title?: string,
+  description: string = siteConfig.description,
+): Metadata => {
+  return {
+    title: title ? `${title} - Email Builder` : title,
+    description,
+  };
+};
