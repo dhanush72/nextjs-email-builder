5a56f6680eb14378d2e9ae2bac82479a7e4343df
dhanush72 <dhanuraj7258@gmail.com>
2024-02-21 09:58:17 +0530

    :lipstick: Added table for folders listing

--- src/app/(dashboard)/dashboard/folder/layout.tsx
+++ src/app/(dashboard)/dashboard/folder/layout.tsx
@@ -3,10 +3,12 @@ import Breadcrumb from '@/components/shared/breadcrumb';
 const breadcrumbItems = [{ title: 'Email', link: '/dashboard' }];
 const EmailLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex-1 space-y-4 p-4 pt-6 md:p-6">
-      <Breadcrumb items={breadcrumbItems} />
-      {children}
-    </div>
+    <>
+      <div className="flex-1 p-4 pt-6 md:p-6">
+        <Breadcrumb items={breadcrumbItems} />
+        {children}
+      </div>
+    </>
   );
 };
 
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,11 @@
-import { auth } from '@/app/api/auth/auth';
+import Header from '@/components/shared/header';
+import FolderTable from '@/components/tables/folder-table';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import { Separator } from '@/components/ui/separator';
 import { metaObject } from '@/config/site.config';
 import { currentUser } from '@/lib/auth';
 import { getFolders } from '@/lib/queries';
 import CreateFolderButton from './_components/create-folder-button';
-import FolderItem from './_components/folder';
 
 export const metadata = {
   ...metaObject('Dashboard'),
@@ -17,34 +18,33 @@ const DashboardPage = async () => {
   const folders = await getFolders(user.id);
 
   return (
-    <ScrollArea className="h-full">
-      <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Folders</h2>
-          <div className="hidden items-center space-x-2 md:flex">
-            <CreateFolderButton label="Add folder" />
-          </div>
-        </div>
-
-        {folders?.length ? (
-          <div className="grid grid-cols-1 gap-2 md:grid-cols-3">
-            {folders.map((folder) => (
-              <FolderItem key={folder.id} folder={folder} />
-            ))}
-          </div>
-        ) : (
-          <div className="flex h-[400px] items-center justify-center bg-gray-50">
-            <div className="flex flex-col items-center">
-              <h5 className="text-xl font-medium">No folders</h5>
-              <p className="mb-2 text-sm text-muted-foreground">
-                Get started by creating a new folder
-              </p>
-              <CreateFolderButton label="Create new folder" />
+    <>
+      <ScrollArea className="h-full">
+        <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-3xl font-bold tracking-tight">Folders</h2>
+            <div className="hidden items-center space-x-2 md:flex">
+              <CreateFolderButton label="Add folder" />
             </div>
           </div>
-        )}
-      </div>
-    </ScrollArea>
+          <Separator />
+
+          {folders?.length ? (
+            <FolderTable folders={folders} />
+          ) : (
+            <div className="flex h-[400px] items-center justify-center bg-gray-50">
+              <div className="flex flex-col items-center">
+                <h5 className="text-xl font-medium">No folders</h5>
+                <p className="mb-2 text-sm text-muted-foreground">
+                  Get started by creating a new folder
+                </p>
+                <CreateFolderButton label="Create new folder" />
+              </div>
+            </div>
+          )}
+        </div>
+      </ScrollArea>
+    </>
   );
 };
 
